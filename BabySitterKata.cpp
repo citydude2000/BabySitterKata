6425ae31de7be52d52a735639a4e20d69210bac2
citydude2000 <35429926+citydude2000@users.noreply.github.com>
2019-05-29 15:01:58 -0500

    Second commit - added code to resolve failed case

--- BabySitterKata.cpp
+++ BabySitterKata.cpp
@@ -8,26 +8,24 @@ class BabySitterPayCalculator
 {
 
 protected:
-	int  ConvertStartTimeToStartIndex(double StartTime)
+	// Input:  dTime = double value 5.00(AM) to 4:00(AM), 
+	//         bStart = true if StartTime, otherwise false 
+	// Output: integer = an index from 0(5PM) to 10(3AM)
+	int  ConvertTimeToIndex(double dTime, bool bStart)
 	{
+		double intPart;
+		//working 5 to 8 is ONLY 3 hours, 
+		if (!bStart && (modf(dTime, &intPart) == 0))
+			dTime = dTime - (double)1.00; //so ie. stoptime 1:00 is really hour 12
+		int Index = 0;
 		//fix the time from 5 to 4 to be index 0 to 11
-		if ((StartTime >= 5) && (StartTime < 12.59))
-			StartTime = StartTime - 5;
+		if ((dTime >= 5) && (dTime < 12.59))
+			Index = (int)dTime - 5;
 		else
-			StartTime = StartTime + 7;
-		return (int)StartTime;
+			Index = (int)dTime + 7;
+		return Index;
 	}
-	
-	int  ConvertStopTimeToStopIndex(double StopTime)
-	{
-		//fix the time from 5 to 4 to be index 0 to 11
-		if ((StopTime >= 5) && (StopTime < 12.59))
-			StopTime = StopTime - 5;
-		else
-			StopTime = StopTime + 7;
-		return (int)StopTime;
-	}
-	
+
 	//using this table under the assumption that the sitter is paid based on a full
 	//hour of pay for the time sitting starts and for a full hour for any partial hours worked
 	int  PayRates[3][11] = { 15, 15, 15, 15, 15, 15, 20, 20, 20, 20, 20,   //family 1
@@ -45,9 +43,9 @@ public:
 	int CalculateTotalPay(double StartTime, double StopTime, int FamilyID)
 	{
 		int TotalBabySitterPay = 0;
-		int StartIndex = ConvertStartTimeToStartIndex(StartTime);
-		int StopIndex = ConvertStopTimeToStopIndex(StopTime);
-		for (int i = StartIndex; i < StopIndex; i++)
+		int StartIndex = ConvertTimeToIndex(StartTime, true);
+		int StopIndex = ConvertTimeToIndex(StopTime, false);
+		for (int i = StartIndex; i <= StopIndex; i++)
 			TotalBabySitterPay = TotalBabySitterPay + PayRates[FamilyID][i];
 		return TotalBabySitterPay;
 	}
@@ -84,11 +82,11 @@ int main()
 {
 	int TotalPay = 0;
 	std::cout << "Hello BabySitter!\n";
-	
+
 	BabySitterPayCalculator * Calculator = new BabySitterPayCalculator();
 	TotalPay = Test5x7x2(Calculator);
 	TotalPay = Test5x1x2(Calculator);
-	//TotalPay = Test5x1p2x2(Calculator); //Next add code to make this pass
+	TotalPay = Test5x1p2x2(Calculator); //Next add code to make this pass
 
 	delete Calculator;
 	std::getchar();
